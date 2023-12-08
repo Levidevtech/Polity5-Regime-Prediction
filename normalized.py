ab9eacf6510c90776b176944695b14b0e144474f
Sharoek <sharoek@live.nl>
2023-12-08 01:10:25 +0100

    added country and year

--- normalized.py
+++ normalized.py
@@ -10,10 +10,9 @@ class Normalize:
         polity5 = pd.read_csv(file_path)
 
         # Exclude country and year to get the min value
-        columns_to_exclude = ['country', 'year']
+        columns_to_exclude = ['country','year']
         excluded_data = polity5.loc[:, ~polity5.columns.isin(columns_to_exclude)]
         min_values = excluded_data.min()
-        max_values = excluded_data.max()
 
         # Shift data
         shifted_data = excluded_data - min_values
@@ -22,11 +21,16 @@ class Normalize:
         scaler = Normalizer().fit(shifted_data)
         normalizedX = scaler.transform(shifted_data)
 
+        # Create a DataFrame with normalized data and include 'country' column
+        normalized_df = pd.DataFrame(data=normalizedX, columns=shifted_data.columns)
+        normalized_df['country'] = polity5['country']  # Add 'country' column back
+        normalized_df['year'] = polity5['year']
+
         # Summarize transformed data
         np.set_printoptions(precision=2)
-        print(normalizedX[0:6, :])
+        print(normalized_df.head(6))
 
-        return normalizedX
+        return normalized_df
 
 
 n = Normalize()
